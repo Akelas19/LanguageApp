6a90e579a12c2637ec0d447d53e3f539bf597622
Akelas <ahohnm.ahohnm@mail.ru>
2024-11-12 19:15:02 +0700

    Initial Commit

---
+++ LanguageApp/LanguageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanguageAppApp.swift
+//  LanguageApp
+//
+//  Created by Александр Переславцев on 12.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct LanguageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
